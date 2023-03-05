1097dbafaf87fd11097e5ef72b7f10b953d2b1c5
emilio-cristobal <ecris1502@gmail.com>
2023-03-04 20:12:02 -0600

    Input de reconocimiento agregado

---
+++ src/components/SpeechRecognitionInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex">
+    <input
+      class="form form-control me-1"
+      v-model="speechText"
+      @input="emit('update:modelValue', $event.target.value)"
+    />
+    <button class="btn btn-primary" @click="startSpeechRecognition">
+      <i class="fa-solid fa-microphone"></i>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue'
+
+const speechText = ref('')
+const emit = defineEmits(['update:modelValue', 'finished'])
+
+const startSpeechRecognition = () => {
+  const recognition = new webkitSpeechRecognition()
+  recognition.lang = 'es'
+  recognition.onresult = (event) => {
+    speechText.value = event.results[0][0].transcript
+    emit('update:modelValue', speechText.value)
+    emit('finished', speechText.value)
+  }
+  recognition.start()
+}
+</script>
